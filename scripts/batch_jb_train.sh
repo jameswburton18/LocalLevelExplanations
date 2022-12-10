bde7167c55b38758a7a4bd4a573b24e36c38d359
James Burton <james.burton@durham.ac.uk>
2022-12-10 15:40:22 +0000

    m,imor

--- scripts/batch_jb_train.sh
+++ scripts/batch_jb_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for cfg in essel ord_first ft_first
+for cfg in essel_2 ord_first_2 ft_first_2
 do
     sbatch --job-name=$cfg scripts/jb_train.sh $cfg
     sleep 8 # so that wandb runs don't get assigned the same number
