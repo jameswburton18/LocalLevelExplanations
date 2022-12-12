d3baad4197b95f37ce8587824de427057988ec7b
James Burton <james.burton@durham.ac.uk>
2022-12-12 11:25:04 +0000

    Big GPU

--- scripts/jb_train_qa.sh
+++ scripts/jb_train_qa.sh
@@ -10,14 +10,14 @@
 #SBATCH --qos long-high-prio
 #SBATCH -t 04-00:00
 # -x shows which ones to ignore
-#SBATCH -x gpu[7,8,10,11,12]
+#SBATCH -x gpu[0-8] #[7,8,10,11,12]
 
 # Job name appears in the squeue output, output is the output filename 
 #SBATCH -o ncc-logs/%x-%A.out
 
 # Pick how much memory to allocate per CPU core.
 #SBATCH --mem 8G
-#SBATCH --gres gpu
+#SBATCH --gres=gpu:ampere:1 #--gres gpu for normal, --gres=gpu:ampere:1 for whole 80gb card
 
 source ./env/bin/activate
 
